63a859afa3884e6e58a628e3be207b0686756f36
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-22 01:36:46 +0100

    add get-photo-url and addPhoto function

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import getPhotoUrl from 'get-photo-url'
 import { posts } from '../data'
 
 const Gallery = () => {
+  const addPhoto = async () => {
+    const imageUrl = await getPhotoUrl('#addPhotoInput')
+    const post = {
+      id: '',
+      imageUrl,
+      text: ''
+    }
+  }
+
   const galleryItems = posts.map(post => (
     <div className="item" key={post.id} tabIndex={8}>
       <img src={post.imageUrl} alt="" />
@@ -12,7 +22,7 @@ const Gallery = () => {
     <>
       <input type="file" name="photo" id="addPhotoInput" />
       <label htmlFor="addPhotoInput">
-        <i className="add-photo fas fa-plus-square" />
+        <i className="add-photo fas fa-plus-square" onClick={addPhoto}/>
       </label>
       <div className="gallery-nav" />
       <section className="gallery">
